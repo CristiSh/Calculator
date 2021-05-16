57f62fc24b63874d3eae56967c3c27eaf8c66f07
Cristi Seican <cristianseican@gmail.com>
2021-05-16 19:12:22 +0300

    added buttons + basic operations + made a script test that works

---
+++ operations.js
@@ -0,0 +1,18 @@
+function add(a,b){
+    return a+b;
+}
+function sub(a,b){
+    return a-b;
+}
+function mul(a,b){
+    return a*b;
+}
+function div(a,b){
+    return a/b;
+}
+function eq(res){
+    alert(res);
+}
+function test(){
+    alert("merge");
+}
