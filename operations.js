9a476db257785846eed752f6c254a9d6be484917
Cristi Seican <cristianseican@gmail.com>
2021-05-19 11:48:49 +0300

    changed entire script

--- operations.js
+++ operations.js
@@ -1,86 +1,39 @@
 const calculator = {
-    displayVal = '0',
-    firstOperand = null,//initial value
-    waitingForSecondOperand: false, //is the first operand and the operator inputed?
+    displayValue: '0',
+    firstOperand : null,//initial value
+    waitingForSecondOperand: false, //checks if the first operand and the operator is inputed
     operator: null,//initial value
 
 };
 function updateDisplay(){
-    const display = document.querySelector('.output');
-    display.value=calculator.displayVal;
+    const display = document.querySelector('.output');//gets all the elements with the class output
+    display.value=calculator.displayValue;
 }
 updateDisplay();
 
+const keys = document.querySelector('.number,.sign');
+keys.addEventListener('click',(event)=>{//access the clicked element
+    const {target} = event; //the element that triggered the event (what we click, in this case); equiv to const target = event.target;
+    // const {value} = target;
 
+    if(!target.matches('button')){
+    return;
+}
 
-
-
-
-
-
-// function sub(a, b) {
-//     return a - b;
+// switch(value){
+//     case '+':
+//     case '-':
+//     case '*':
+//     case '/':
+//     case '=':
 // }
 
-// function mul(a, b) {
-//     return a * b;
-// }
 
-// function div(a, b) {
-//     return a / b;
-// }
+if(target.classList.contains('sign')){
+    console.log('sign',target.value);
+    return;
+}
+console.log('digit',target.value);
 
-// function eq(res) {
-//     alert(res);
-// }
-
-// function test() {
-//     alert("merge");
-// }
-
-// function display() {
-//     document.getElementById("one").addEventListener("click", function () {
-//         document.getElementById("operator1").innerHTML = 1;
-//     });
-//     document.getElementById("two").addEventListener("click", function () {
-//         document.getElementById("operator1").innerHTML = 2;
-//     });
-//     document.getElementById("three").addEventListener("click", function () {
-//         document.getElementById("operator1").innerHTML = 3;
-//     });
-//     document.getElementById("four").addEventListener("click", function () {
-//         document.getElementById("operator1").innerHTML = 4;
-//     });
-//     document.getElementById("five").addEventListener("click", function () {
-//         document.getElementById("operator1").innerHTML = 5;
-//     });
-//     document.getElementById("six").addEventListener("click", function () {
-//         document.getElementById("operator1").innerHTML = 6;
-//     });
-//     document.getElementById("seven").addEventListener("click", function () {
-//         document.getElementById("operator1").innerHTML = 7;
-//     });
-//     document.getElementById("eight").addEventListener("click", function () {
-//         document.getElementById("operator1").innerHTML = 8;
-//     });
-//     document.getElementById("nine").addEventListener("click", function () {
-//         document.getElementById("operator1").innerHTML = 9;
-//     });
-//     document.getElementById("zero").addEventListener("click", function () {
-//         document.getElementById("operator1").innerHTML = 0;
-//     });
-// }
-// //we create an array where we put all values,
-// //then we add all of them (for addition);
-// //the array elements will be input from the calculator buttons
-// var a = [];
-// var i = 0;
-// var sum = 0;
-// function add(){
-//     a[i]=document.getElementById("operator1").value;//add number in array from calculator interface
-//     i++;
-//     for(var j = 0;j<a.length;j++){
-//         sum = sum + a[j];
-//     }
-//     console.log(sum) ;
-// }
+}
+);
